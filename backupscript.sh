d1fb811ad55d6a40905eeac55393861fe84ccdad
schwarzsascha <magni@magnis-MacBook-Pro.local>
2014-06-02 15:20:17 +0200

    Backupscript Funktionen hinzugefügt

--- backupscript.sh
+++ backupscript.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 XAMPP_PATH="/Applications/XAMPP"
 GIT_PATH="/Applications/GitHub.app/Contents/Resources/git/bin"
-DUMP_FILENAME="mysqldump.sql"
+DUMP_FILEPREFIX="mysqldump"
+DATABASES=("web1" "web2")
+MYSQL_USER="root"
+MYSQL_PASS=""
 
-${XAMPP_PATH}/xamppfiles/bin/mysqldump -l --all-databases > ${DUMP_FILENAME}
-${GIT_PATH}/git add ${DUMP_FILENAME}
-${GIT_PATH}/git add $0
-${GIT_PATH}/git commit -m "Autocommit durch Script vom `date`"
+function backup {
+    for DB_NAME in ${DATABASES}; do
+        DUMP_FILENAME=${DUMP_FILEPREFIX}_${DB_NAME}.sql
+        ${XAMPP_PATH}/xamppfiles/bin/mysqldump -l ${DB_NAME} > ${DUMP_FILENAME}
+        ${GIT_PATH}/git add ${DUMP_FILENAME}
+    done
+    ${GIT_PATH}/git add $0
+    ${GIT_PATH}/git commit -m "Autocommit durch Script vom `date`"
+}
+
+function import {
+    for DB_NAME in ${DATABASES}; do
+        DUMP_FILENAME=${DUMP_FILEPREFIX}_${DB_NAME}.sql
+        ${XAMPP_PATH}/xamppfiles/bin/mysql -u${MYSQL_USER} -p${MYSQL_USER} ${DB_NAME}  > ${DUMP_FILENAME}
+    done
+}
