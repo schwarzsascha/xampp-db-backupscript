7f6e7f48234eb55ef28226968c2108afedc0f022
schwarzsascha <magni@magnis-MacBook-Pro.local>
2014-06-02 15:31:28 +0200

    Abfrage und Parameterübergabe

--- backupscript.sh
+++ backupscript.sh
@@ -19,6 +19,37 @@ function backup {
 function import {
     for DB_NAME in ${DATABASES}; do
         DUMP_FILENAME=${DUMP_FILEPREFIX}_${DB_NAME}.sql
-        ${XAMPP_PATH}/xamppfiles/bin/mysql -u${MYSQL_USER} -p${MYSQL_USER} ${DB_NAME}  > ${DUMP_FILENAME}
+        ${XAMPP_PATH}/xamppfiles/bin/mysql -u${MYSQL_USER} -p${MYSQL_USER} ${DB_NAME} < ${DUMP_FILENAME}
     done
 }
+
+function usage {
+    echo Hier kommt die anleitung rein!!!
+}
+
+function menu {
+    OPTIONS="Backup Import"
+    select opt in $OPTIONS; do
+        if [ "$opt" = "Backup" ]; then
+            backup()
+        elif [ "$opt" = "Import" ]; then
+            import()
+        else
+            clear
+            echo Bad Option
+            menu()
+        fi
+    done
+}
+
+if [ -z "$1" ]; then
+    menu()
+else
+    if [ $1 == "-b" ]; then
+        backup()
+    elif [ $1 == "-i" ]
+        import()
+    else
+        usage()
+    fi
+fi
